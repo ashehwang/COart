e03a92ea12a7136c8988107f8ae9128728a9e334
Ashe <ashe@nyu.edu>
2020-10-06 13:36:27 -0700

    saving before changing

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -181,12 +181,14 @@ class NavBar extends React.Component {
 
         return (
           <nav className="navbar-container">
-            <div className="navbar-search-container">
+            <div className="navbar-search-container relative">
+              <img className="absolute navbar-logo" src="https://i.ibb.co/fYkKYp5/try3.png" alt="try3" border="0"></img>
               {/* <div className="hover" onClick={() => this.props.history.push("/main")}><img src="https://i.ibb.co/tMkrDPR/Untitled.png" /></div> */}
               <div className="hover" onClick={() => this.props.history.push("/")}><img src="https://i.ibb.co/y8hYN50/cotellimage.png" /></div>
               <div className="flex">
                 <i className="fas fa-search white"></i>
                 <SearchBarContainer />
+                <i className="fas fa-home white absolute hover" onClick={() => this.props.history.push("/main")}></i>
               </div>
             </div>
             <div className="navbar-menu flex">
